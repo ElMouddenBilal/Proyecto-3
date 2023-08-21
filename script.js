cc54a4004938b67718a6f2437c7fcbba13a8d443
elmouddenbilal04@gmail.com <elmouddenbilal04@gmail.com>
2023-08-21 18:22:11 +0200

    mejorado

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
+// Seleccionar todas las secciones y enlaces de navegación en el encabezado
 let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("header nav a");
 
+// Configurar el evento de desplazamiento en la ventana
 window.onscroll = () => {
+    // Obtener la posición de desplazamiento vertical
+    let top = window.scrollY;
+
+    // Iterar a través de cada sección
     sections.forEach(sec => {
-        let top = window.scrollY;
+        // Obtener la posición superior de la sección con una compensación de 150px
         let offset = sec.offsetTop - 150;
+        // Obtener la altura de la sección
         let height = sec.offsetHeight;
+        // Obtener el atributo 'id' de la sección
         let id = sec.getAttribute("id");
 
+        // Verificar si la posición de desplazamiento está dentro de los límites de la sección actual
         if (top >= offset && top < offset + height) {
-            navLinks.forEach(links=> {
-                links.classList.remove("active");
-                document.querySelector("header nav a [href*=" + id + "]").classList.add("active");
+            // Iterar a través de todos los enlaces de navegación
+            navLinks.forEach(link => {
+                // Eliminar la clase 'active' de todos los enlaces
+                link.classList.remove("active");
+                
+                // Verificar si el atributo 'href' del enlace contiene el 'id' de la sección actual
+                if (link.getAttribute("href") === "#" + id) {
+                    // Agregar la clase 'active' al enlace que coincide con la sección actual
+                    link.classList.add("active");
+                }
             });
-        };
+        }
     });
 };
